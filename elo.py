540fd7e780274c14dd50f61ac437a6d27c0b2bb4
Andrey Ryabov <ghfghfg23@gmail.com>
2014-04-18 18:17:06 +0400

    add EloRatingObject

--- elo.py
+++ elo.py
@@ -1,8 +1,9 @@
+class EloRatingObject:
+	def __init__(self):
+		self.commonRating = 1000
+		self.runnerRating = 1000
+		self.corpRating = 1000
 
-class EloResult:
-    WIN = 1
-    DRAW = 2
-    LOSE = 3
 
 class EloRatingSystem:
 	def __init__(self):
@@ -20,66 +21,82 @@ class EloRatingSystem:
 			pass
 		putStringForId(pId)
 		# putStringForId(p2Id)
-		
 
-	def calculateGame(self, player1Id, p1CorpPoints, p2RunnerPoints, p1RunnerPoints, p2CorpPoints, player2Id):
+	# def _
+
+	def _getRatingTupleForIds(self, player1Id, player2Id):
 		if player1Id in self.ratingTable:
-			ratingA = self.ratingTable[player1Id]
+			ratingAObject = self.ratingTable[player1Id]
 			self.playedGames[player1Id] += 1
 		else:
-			ratingA = 1000
+			ratingAObject = EloRatingObject()
 			self.playedGames[player1Id] = 1
 
 		if player2Id in self.ratingTable:
-			ratingB = self.ratingTable[player2Id]
+			ratingBObject = self.ratingTable[player2Id]
 			self.playedGames[player2Id] += 1
 		else:
-			ratingB = 1000
+			ratingBObject = EloRatingObject()
 			self.playedGames[player2Id] = 1
+		return (ratingAObject, ratingBObject)
 
-		scoreA = 0.0
-		scoreB = 0.0
+	def calculateGame(self, player1Id, p1CorpPoints, p2RunnerPoints, p1RunnerPoints, p2CorpPoints, player2Id):
+		ratingTuple = self._getRatingTupleForIds(player1Id, player2Id)
+		ratingAObject = ratingTuple[0]
+		ratingBObject = ratingTuple[1]
+		runScoreA = 0.0
+		corpScoreA = 0.0
+
+		runScoreB = 0.0
+		corpScoreB = 0.0
+
+		ratingA = ratingAObject.commonRating
+		ratingB = ratingBObject.commonRating
+
 		if p1CorpPoints > p2RunnerPoints: 
-			scoreA += 1
+			corpScoreA += 1
 			if p1CorpPoints > 6:
-				scoreA += 1
+				corpScoreA += 1
 		elif p2RunnerPoints > p1CorpPoints:
-			scoreB += 1
+			runScoreB += 1
 			if p2RunnerPoints > 6:
-			 	scoreB += 1
-		elif p2RunnerPoints == p1CorpPoints:# or (p2RunnerPoints < 7 and p1CorpPoints < 7):
-			scoreA += 1
-			scoreB += 1
+			 	runScoreB += 1
+		elif p2RunnerPoints == p1CorpPoints:
+			corpScoreA += 1
+			runScoreB += 1
 
 		if p1RunnerPoints > p2CorpPoints:
-			scoreA += 1
+			runScoreA += 1
 			if p1RunnerPoints > 6:
-				scoreA += 1
+				runScoreA += 1
 		elif p2CorpPoints > p1RunnerPoints:
-			scoreB += 1
+			corpScoreB += 1
 			if p2CorpPoints > 6:
-			 	scoreB += 1
-		elif p2CorpPoints == p1RunnerPoints:# or (p2CorpPoints < 7 and p1RunnerPoints < 7):
-			scoreA += 1
-			scoreB += 1
+			 	corpScoreB += 1
+		elif p2CorpPoints == p1RunnerPoints:
+			runScoreA += 1
+			corpScoreB += 1
+
+		scoreA = runScoreA + corpScoreA
+		scoreB = runScoreB + corpScoreB
 
 		D = (4.0 - scoreA - scoreB )/8
 		scoreA = scoreA / 4.0 + D
 		scoreB = scoreB / 4.0 + D
-		# print "A: %.2f B: %.2f" % (scoreA, scoreB)
-		# print "D = %.2f" % (D)
 
 		relativeScoreA = scoreA / (scoreA + scoreB)
 		relativeScoreB = 1 - relativeScoreA
+
+		relativeRunScoreA = runScoreA / (runScoreA + corpScoreB)
 		# print "rA: %.2f rB: %.2f" % (relativeScoreA, relativeScoreB)
 
-		newRA = self._recalculateEvo(ratingA, ratingB, relativeScoreA, 20)
-		newRB = self._recalculateEvo(ratingB, ratingA, relativeScoreB, 20)
-		self.ratingTable[player1Id] = newRA
-		self.ratingTable[player2Id] = newRB
+		ratingAObject.commonRating = self._recalculateEvo(ratingA, ratingB, relativeScoreA, 20)
+		ratingBObject.commonRating = self._recalculateEvo(ratingB, ratingA, relativeScoreB, 20)
+		self.ratingTable[player1Id] = ratingAObject
+		self.ratingTable[player2Id] = ratingBObject
 
-		dA = newRA - ratingA
-		dB = newRB - ratingB
+		dA = ratingAObject.commonRating - ratingA
+		dB = ratingBObject.commonRating - ratingB
 		# historyStringA = "(%.2f %+.3f) %s (%i/%i = %.2f) - (%i/%i = %.2f) %s (%.2f %+.3f)" %(ratingA, dA, player1Id, p1CorpPoints, p1RunnerPoints, scoreA, p2RunnerPoints, p2CorpPoints, scoreB, player2Id, ratingB, dB)
 		# historyStringB = "(%.2f %+.3f) %s (%i/%i = %.2f) - (%i/%i = %.2f) %s (%.2f %+.3f)" %(ratingB, dB, player2Id, p2CorpPoints, p2RunnerPoints, scoreB, p1RunnerPoints, p1CorpPoints, scoreA, player1Id, ratingA, dA)
 		historyStringA = "(%.2f %+.3f) %s (%i/%i) - (%i/%i) %s (%.2f %+.3f)" %(ratingA, dA, player1Id, p1CorpPoints, p1RunnerPoints, p2RunnerPoints, p2CorpPoints, player2Id, ratingB, dB)
@@ -105,8 +122,8 @@ class EloRatingSystem:
 		return strings
 
 	def ratingTableList(self):
-		sortedTable = sorted(self.ratingTable.keys(), key=lambda x: self.ratingTable[x] * -1)
+		sortedTable = sorted(self.ratingTable.keys(), key=lambda x: self.ratingTable[x].commonRating * -1)
 		strings = []
 		for name in sortedTable:
-			strings.append(str(name.encode("utf-8")) + "\t%.2f" % self.ratingTable[name] + "\t%i" % self.playedGames[name])
+			strings.append(str(name.encode("utf-8")) + "\t%.2f" % self.ratingTable[name].commonRating + "\t%i" % self.playedGames[name])
 		return strings
