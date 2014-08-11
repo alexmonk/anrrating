7a9fd5ce6f1c81b1182b7b9b7bbd3f1bf706274b
alexmonk <pedro87@mail.ru>
2014-08-12 03:05:49 +0400

    Create rating_least_squares.py
    Draft for least squares k-level rating

---
+++ rating_least_squares.py
@@ -0,0 +1,42 @@
+#-*- coding: utf-8 -*-
+from power_level import PowerLevel
+from tournament import createPlayersList
+
+#------------------------------------------------------------------
+
+class P2PResult:
+	def __init__(self):
+		self.wins = 0.
+		self.loses = 0.
+
+	def update(self, wins, loses):
+		self.wins += wins
+		self.loses += loses
+
+#------------------------------------------------------------------
+
+class LeastSquaresRating:
+	def __init__(self, matches):
+		players = createPlayersList(matches)
+		self.results = dict()
+		for playerA in players:
+			self.results[playerA] = dict()
+			for playerB in players:
+				self.results[playerA][playerB] = P2PResult()
+
+		for match in matches:
+			playerA = match.playerA
+			playerB = match.playerB
+			(winA, winB) = match.calculateWins()
+			self.results[playerA][playerB].update(winA, winB)
+			self.results[playerB][playerA].update(winB, winA)
+
+#------------------------------------------------------------------
+
+	def calculatePowers(self, levels):
+		powers = dict()
+		for player in self.results.keys():
+			powers[player] = PowerLevel(levels)
+
+
+#------------------------------------------------------------------
