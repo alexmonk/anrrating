f994840a70bb98379e8609e203b40988d9c61a28
alexmonk <pedro87@mail.ru>
2014-08-12 03:04:05 +0400

    Update test_elo.py
    Added double elimination

--- test_elo.py
+++ test_elo.py
@@ -1,16 +1,16 @@
-from elo import EloRatingSystem
+#-*- coding: utf-8 -*-
+import xmltodict
+import os
+from datetime import datetime
 
-eloRatingSystem = EloRatingSystem()
-eloRatingSystem.calculateGame("4:0", 10, 0, 10, 0, "0:4")
-eloRatingSystem.calculateGame("3:1", 10, 0, 3, 3, "1:3")
-eloRatingSystem.calculateGame("2:2/1", 10, 0, 0, 10, "2:2/2")
-eloRatingSystem.calculateGame("3:0", 10, 0, 5, 0, "0:3")
-eloRatingSystem.calculateGame("2:1", 10, 0, 0, 4, "1:2")
-eloRatingSystem.calculateGame("1:0", 3, 0, 0, 0, "0:1")
-eloRatingSystem.calculateGame("1:1/1", 3, 0, 0, 3, "1:1/2")
+from tournament import Tournament
+from tournament import createPlayersList
+from rating_elo import EloRating
+from rating_least_squares import LeastSquaresRating
 
-history = eloRatingSystem.historyStringDict()
-for player in history.keys():
-	historyString = history[player]
-	if historyString != "(ratingA +/- deltaA) nameA (corpA/runnerA) - (runnerB, corpB) nameB (ratingB +/- deltaB)\r\n":
-		print historyString
+# -----------------------------------------------------------------
+def testTournamentParse():
+	pass
+
+if __name__ == "__main__":
+	testTournamentParse()
